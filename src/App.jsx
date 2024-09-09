96fad43c2877c72fbadcca0efafaa4fa2e52a554
GREGWILL\Grego <gregorywilliamm@gmail.com>
2024-09-09 12:04:13 -0400

    finalizando el programa

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({x: 0, y: 0})
+  useEffect(()=>{
+    console.log('efecto', {enabled})
+
+    const handleMove = (event) => {
+      const {clientX, clientY} = event
+      console.log('handleMove', {clientX, clientY});
+      setPosition({x: clientX, y: clientY})
+      
+    }
+    if (enabled){
+      window.addEventListener('pointermove', handleMove)
+
+    }
+    // cleanup:
+    // -> cuando el componente se desmonta
+    // -> cuando cambian las dependencias, antes de ejecutar
+    //    el efecto de nuevo
+    return ()=>{
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
 
   return (
-    <>
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}>
+
+      </div>
       <h3>Mouse Follower</h3>
-    </>
+      <button onClick={()=>{setEnabled(!enabled)}}>{enabled ? 'desactivar' : 'activar '} mouse</button>
+    </main>
   )
 }
 
